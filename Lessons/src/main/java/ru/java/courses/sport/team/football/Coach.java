88031367e0c600c01e3e69a24473e43e96479cd8
konev <konev@reksoft.ru>
2019-03-26 20:17:18 +0300

    lesson 5-8 hw part 1

--- Lessons/src/main/java/ru/java/courses/sport/team/football/Coach.java
+++ Lessons/src/main/java/ru/java/courses/sport/team/football/Coach.java
@@ -2,7 +2,40 @@ package ru.java.courses.sport.team.football;
 
 public class Coach {
 
+    // Поля для тренера:
+    // name - имя тренера
+    // experience - опыт тренера
     private String name;
     private Byte experience;
 
+    // Геттер имени тренера
+    public String getName() {
+        return name;
+    }
+
+    // Сеттер имени тренера
+    public void setName(String name) {
+        if ((name == null)||(name.isEmpty())) {
+            throw new IllegalArgumentException("Значение Имя не может быть пустым");
+        }
+        this.name = name;
+    }
+
+    // Геттер опыта тренера
+    public Byte getExperience() {
+        return experience;
+    }
+
+    // Сеттер Опыта тренера
+    public void setExperience(Byte experience) {
+        this.experience = experience;
+    }
+
+    // Переопределяем преобразование объекта типа "Тренер" в стоку
+    @Override
+    public String toString() {
+        return "Тренер: " +
+                "Имя: " + name + '\'' +
+                "Опыт: " + experience;
+    }
 }
--- Lessons/src/main/java/ru/java/courses/sport/team/football/Team.java
+++ Lessons/src/main/java/ru/java/courses/sport/team/football/Team.java
@@ -1,25 +1,85 @@
 package ru.java.courses.sport.team.football;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Team {
 
-    private Array players;
+    // Поля для команды:
+    // players - игроки (массив объектов типа ScoringPlayers) команды
+    // name - название команды
+    // coach - тренер команды
+    private ArrayList<ScoringPlayer> players = new ArrayList<>();
     private String name;
+
     private Coach coach;
 
+    // Метод добавления игроков в команду
     public void addPlayers(ScoringPlayer scoringPlayer) {
-        if (players.length<20) {
-            this.players = ;
+        if (players.size() > 20){
+            throw new IllegalArgumentException("В команде не может быть больше 20 игроков");
+        } else {
+            players.add(scoringPlayer);
         }
     }
 
+    // Метод удаления игроков из команды
+    public void removePlayers(ScoringPlayer scoringPlayer) {
+        if (players.size() < 0) {
+            throw new IllegalArgumentException("В команде отсутствуют игроки, удалить игроков нельзя");
+        } else {
+            players.remove(scoringPlayer);
+        }
+    }
+
+    // Метод получения игроков количества игроков в команде
     public int getPlayersCount() {
-        return 0;
+        return players.size();
     }
 
-    public String getScore() {
-        return null;
+    // Метод получения количества забитых командой голов
+    public byte getScore() {
+        byte score = 0;
+
+        for (int i=0; i - players.size() == 0; i++) {
+            score += players.get(i).getScore();
+        }
+        return score;
     }
 
+    // Метод получения названия команды
+    public String getName() {
+        return name;
+    }
+
+    // Метод установления названия команды
+    public void setName(String name) {
+        if ((name == null) || (name.isEmpty())) {
+            throw new IllegalArgumentException("Название команды не может быть пустым");
+        }
+        this.name = name;
+    }
+
+    // Метод получения тренера команды
+    public Coach getCoach() {
+        return coach;
+    }
+
+    // Метод установления тренера команды
+    public void setCoach(Coach coach) {
+        if (coach == null) {
+            throw new IllegalArgumentException("Значение Тренер не может быть пустым");
+        }
+        this.coach = coach;
+    }
+
+    // Переопределяем преобразование объекта типа "Команда" в стоку
+    @Override
+    public String toString() {
+        return "Команда: " +
+                "Название: " + name +
+                "Количество игроков: " + getPlayersCount() +
+                "Игроки: " + players + '\'' +
+                "Тренер: " + coach +
+                "Голов забито: " + getScore();
+    }
 }
